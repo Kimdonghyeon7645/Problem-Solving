4943e1d8c30ec50fb0baa3bafc78298ec8e6b8cd
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-06-06 16:38:10 +0900

    docs : upload 5, 6.py

---
+++ 문제집2-별찍기/5-별찍기,2442번.py
@@ -0,0 +1,3 @@
+m = int(input())
+for i in range(m):
+    print(" " * (m - i - 1) + "*" * (1 + i*2))
---
+++ 문제집2-별찍기/6-별찍기,2443번.py
@@ -0,0 +1,3 @@
+m = int(input())
+for i in range(m):
+    print(" " * i + "*" * ((m - i)*2 - 1))
