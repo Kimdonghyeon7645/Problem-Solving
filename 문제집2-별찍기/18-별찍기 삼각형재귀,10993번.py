29b5ba449fb4d615956fc5c887ccbf7a55a10a9e
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-06-07 19:48:11 +0900

    fix : edit 18.py

--- 문제집2-별찍기/18-별찍기 삼각형재귀,10993번.py
+++ 문제집2-별찍기/18-별찍기 삼각형재귀,10993번.py
@@ -1,10 +1,7 @@
 # 6:45 ~ 7:30, 45분동안 머리좀 싸맸다.
-# 시간을 의식하고 짜다보니까 쫒기는 느낌에 코드가 최적화가 안됬다.,,
-m = int(input())
+# 시간을 의식하고 짜다보니까 쫒기는 느낌에 코드가 최적화가 안됬다,,,
 li = ['*']
-if m == 1:
-    print(li[0])
-for i in range(1, m):
+for i in range(1, int(input())):
     top = []
     bottom = []
     if i % 2 == 1:  # 역삼각형 뚝딱
@@ -13,15 +10,15 @@ for i in range(1, m):
         for index in range(len(li)):
             top.append('*' + ' ' * ((len(li) - 1 - index) * 2 - 1) + '*' * bool(len(li) - 1 - index))
         li = ['*' * (len(top[0]) + 2)] + top + bottom
-        if i == m-1:
-            for id, col in enumerate(li):
-                print(' ' * id + col)
     else:   # 삼각형 뚝딱
         for index in range(len(li)):
             top.append('*' + ' ' * (index * 2 - 1) + '*' * bool(index))
         for index in range(1, len(li)):
             bottom.append('*' + ' ' * (2 * index) + li[index] + ' ' * (2 * index) + '*')
         li = top + ['*' + li[0] + '*'] + bottom + ['*' * (len(bottom[-1]) + 2)]
-        if i == m-1:
-            for id, col in enumerate(li):
-                print(' ' * (len(li) - id - 1) + col)
+
+for id, col in enumerate(li):
+    if li[0] == '*':    # 삼각형 출력
+        print(' ' * (len(li) - id - 1) + col)
+    else:   # 역삼각형 출력
+        print(' ' * id + col)
