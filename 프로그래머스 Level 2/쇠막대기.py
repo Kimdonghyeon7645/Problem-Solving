f95e1e13aa8bff912450131beefaab841c2d43c3
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-06-13 16:22:06 +0900

    docs : upload 쇠막대기.py

---
+++ 프로그래머스 Level 2/쇠막대기.py
@@ -0,0 +1,15 @@
+def solution(arrangement):
+    answer = bar = 0
+    for i in range(len(arrangement)):
+        if arrangement[i] == '(':
+            if arrangement[i+1] == '(':
+                bar += 1
+            else:
+                answer += bar
+        elif arrangement[i-1] == ')':
+            bar -= 1
+            answer += 1
+    return answer
+
+
+print(solution('()(((()())(())()))(())'))
