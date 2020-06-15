0ca0bd7ef35dea7f341683ef332983c0be3dfdeb
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-06-15 19:04:14 +0900

    docs : solved 프린터.py

--- 프로그래머스 Level 2/프린터.py
+++ 프로그래머스 Level 2/프린터.py
@@ -1,30 +1,15 @@
 def solution(priorities, location):
-    # lmax = len(priorities)
-    # if location:
-    #     location -= 1
-    # while True:
-    #     print(priorities)
-    #     top = priorities.pop(0)
-    #     if priorities[0] != max(priorities):
-    #         if location == 0:   # 1이 이 구문에 들어가는 오류
-    #             location += len(priorities)
-    #         priorities.append(top)
-    #     elif location == 0:
-    #         break
-    #     location -= 1
-    # return lmax - len(priorities)
-    m = len(priorities)                     
-    n = priorities[location]                
-    i = priorities.index(max(priorities))   
-    print(i, priorities[i])                 
-    while True:                             
-        i += 1                              
-        if i >= len(priorities):            
-            i = 0                           
-        if priorities[i] == max(priorities):
-            if priorities.pop(i) == n:      
-                break                       
-    return m - len(priorities)              
+    m = len(priorities)
+    while True:
+        if priorities[0] < max(priorities):
+            priorities.append(priorities.pop(0))
+        else:
+            priorities.pop(0)
+            if location == 0:
+                break
+        location = location - 1 if location >= 1 else len(priorities) - 1
+    return m - len(priorities)
 
 
-print(solution([2, 1, 3, 2], 2))
+# 아래는 테스트 코드
+print(solution([2, 1, 3, 2], 0))
