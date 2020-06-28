10840f33a0c8fcbfaae5d0edfddf5f29e081593a
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-06-28 19:22:41 +0900

    docs : update py code

--- 단계별로풀어보기-8/2-숫자의 합,11720번.py
+++ 단계별로풀어보기-8/2-숫자의 합,11720번.py
@@ -1,4 +1,4 @@
 input()
-print(sum(map(int, list(input()))))
+# print(sum(map(int, list(input())))) # 와 같이 해도 되지만,
 # 근데 input()으로 받는 자료형이 str이라, 이미 시퀀스 자료형이므로 list()로 형변환 할 필요 없음
 print(sum(map(int, list(input()))))
