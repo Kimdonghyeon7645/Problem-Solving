ed4ed538f362b3863dcb2f0ee139a8b5b5b1f8d7
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-06-28 19:31:30 +0900

    docs : update py code

--- 단계별로풀어보기-8/3-알파벳 찾기,10809번.py
+++ 단계별로풀어보기-8/3-알파벳 찾기,10809번.py
@@ -2,3 +2,9 @@ w = input()
 for i in range(97, 123):
     print(w.find(chr(i)), end=' ')
 
+# 아래와 같이 해도 가능
+w = input()
+print(*[w.find(chr(i))for i in range(97, 123)], end=' ')
+
+# 숏코딩 모범답안, 이건 인정
+print(*map(input().find, map(chr, range(97, 123))))
