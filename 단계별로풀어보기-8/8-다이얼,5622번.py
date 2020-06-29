14e5c6b9977196c06eca8bb89ee1623fc6a3eb2a
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-06-29 16:51:16 +0900

    docs : upload py code

--- 단계별로풀어보기-8/8-다이얼,5622번.py
+++ 단계별로풀어보기-8/8-다이얼,5622번.py
@@ -1,1 +1,1 @@
-print(sum((ord(i) - 63) // 3 + 2 for i in input()))
+print(sum((ord(i) - 63) // 3 + 3 for i in input()))
---
+++ 프로그래머스 Level 3/N-Queen.py
@@ -0,0 +1,7 @@
+def solution(n):
+    for i in range(n):
+    answer = 0
+    return answer
+
+
+print(solution(2))
