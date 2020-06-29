c5abdf3f046edbd52d256b9189311f1edd50b5ad
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-06-29 18:07:47 +0900

    docs : upload py code

--- 단계별로풀어보기-8/8-다이얼,5622번.py
+++ 단계별로풀어보기-8/8-다이얼,5622번.py
@@ -1,1 +1,3 @@
-print(sum((ord(i) - 63) // 3 + 3 for i in input()))
+word = input()
+dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+print(sum(dial.index(j) + 3 for j in dial for i in word if i in j))
