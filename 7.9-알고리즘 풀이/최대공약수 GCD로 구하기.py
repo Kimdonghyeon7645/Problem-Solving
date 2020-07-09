db752a6b2da8de739ffb2c20b61f0b14feefdc5e
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-07-09 19:12:29 +0900

    docs : create py code

--- 7.9-알고리즘 풀이/최대공약수 GCD로 구하기.py
+++ 7.9-알고리즘 풀이/최대공약수 GCD로 구하기.py
@@ -1,16 +1,19 @@
 # 반복문
-n, m = map(int, input().split())
-n, m = (n, m) if n > m else (m, n)
-while m != 0:
-    n, m = m, n % m
-print(n)
+n1, n2 = map(int, input().split())
+n1, n2 = (n1, n2) if n1 > n2 else (n2, n1)
+while n2 != 0:
+    n1, n2 = n2, n1 % n2
+print(n1)
 
 
 # 재귀함수
-def gcd(a: int, b: int) -> int:
-    if b == 0:
-        return a
-    return gcd(b, a % b)
+def gcd(num1: int, num2: int) -> int:
+    if num2 == 0:
+        return num1
+    return gcd(num2, num1 % num2)
 
 
-print(gcd(n, m))
+print(gcd(n1, n2))
+"""
+유클리드 
+"""
