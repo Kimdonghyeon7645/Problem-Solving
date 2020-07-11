9212e0d5fb8ec964785386209c22c3dfc64c6071
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-07-12 01:14:33 +0900

    docs : upload py code

--- 프로그래머스 Level 1/이상한 문자 만들기.py
+++ 프로그래머스 Level 1/이상한 문자 만들기.py
@@ -3,5 +3,5 @@ def solution(s):
 
 
 print(solution("try hello world"))
-print(solution("a   b  c d eF"))
-print(solution("   a   b  c d eF   "))
+print(solution("a   b  c d eF"))    # 여러개의 공백을 처리해 줄 수 있는지
+print(solution("   a   b  c d eF   "))      # 앞, 뒤의 여러개 공백을 처리해 줄 수 있는지
