791beade6926a97cc5b775eded0ca5822b3dd266
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-07-11 17:56:52 +0900

    docs : update py code

--- 프로그래머스 Level 1/문자열 내 마음대로 정렬하기.py
+++ 프로그래머스 Level 1/문자열 내 마음대로 정렬하기.py
@@ -1,9 +1,17 @@
+# 영 좋지 않은 효율의 함수
+# def solution(strings, n):
+#     li = [[] for i in range(ord('a'), ord('z')+1)]
+#     for yo in strings:
+#         li[ord(yo[n]) - 97].append(yo)
+#     for i in range(ord('a') - 97, ord('z') - 96):
+#         li[i].sort()
+#     return [x for y in li if y != [] for x in y]
+
+
 def solution(strings, n):
-    li = [[] for i in range(ord('a'), ord('z')+1)]
-    for yo in strings:
-        li[ord(yo[n]) - 97].append(yo)
-    print(li)
+    return sorted(sorted(strings), key=lambda strings: strings[n])
 
 
-solution(['sun', 'bed', 'car'], 1)
-solution(['abce', 'abcd', 'cdx'], 2)
+# 아래는 테스트 코드
+print(solution(['sun', 'bed', 'car'], 1))
+print(solution(['abce', 'abcd', 'cdx'], 2))
