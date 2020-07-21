9e01778877603158d896a143e73805a616d04340
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-07-21 19:01:17 +0900

    docs : update py file

---
+++ 알고리즘 수업/7.23/스텍 구현.py
@@ -0,0 +1,28 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def __str__(self):
+        return f"현재 스텍의 상태 : {self.stack}"
+
+    def push(self, num):
+        self.stack += num
+
+    def pop(self):
+        return self.stack.pop()
+
+    def top(self):
+        return self.stack[-1]
+
+    def __sizeof__(self):
+        return len(self.stack)
+
+    def empty(self):
+        if self.stack:
+            return 1
+        else:
+            return 0
+
+
+mystack = Stack()
+print(mystack)
