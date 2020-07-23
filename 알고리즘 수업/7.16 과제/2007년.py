aa7ad7effe3194c7bf7310514f9b459f4baf5aa0
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-07-23 19:21:05 +0900

    docs : update py file

--- 알고리즘 수업/7.16 과제/2007년.py
+++ 알고리즘 수업/7.16 과제/2007년.py
@@ -1,2 +1,2 @@
 a, b = map(int, input().split())
-print(("MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN",)[(sum((31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)[:a-1]) + (b-1)) % 7])
+print(("MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN",)[(sum((31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)[:a-1]) + (b-1)) % 7])
