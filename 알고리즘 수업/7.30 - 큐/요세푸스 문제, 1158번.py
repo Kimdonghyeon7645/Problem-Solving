212d2033d881eca9958baee614075ff734dedc65
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-07-31 08:47:30 +0900

    docs : update py code

--- 알고리즘 수업/7.30 - 큐/요세푸스 문제, 1158번.py
+++ 알고리즘 수업/7.30 - 큐/요세푸스 문제, 1158번.py
@@ -1,19 +1,19 @@
 n, k = map(int, input().split())
 result = []
 
+""" 큐로 했는데 시간 초과인 경우, (2중 반복문이기도 해서..) """
 # qu = [i for i in range(n, 0, -1)]
 # while qu:
 #     for _ in range(k-1):
 #         t = qu.pop()
-#         qu = [t].append(qu)
+#         qu = [t] + qu
 #     result.append(str(qu.pop()))
 # print('<' + ', '.join(result) + '>')
-# 위에는 큐로 짰는데 효율이...
 
+""" 인덱스를 이용한 경우 (num = (num + k - 1) % len(qu) 가 절묘)"""
 num = 0
 qu = [i for i in range(1, n+1)]
 for _ in range(n):
-    num += k-1
-    num %= len(qu)
+    num = (num + k - 1) % len(qu)
     result.append(str(qu.pop(num)))
 print('<' + ', '.join(result) + '>')
