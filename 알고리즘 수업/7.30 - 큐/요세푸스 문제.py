fc8c8f025d6b085967a1ac17d0678b2735fa22cb
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-07-30 22:49:13 +0900

    docs : update py code

--- 알고리즘 수업/7.30 - 큐/요세푸스 문제.py
+++ 알고리즘 수업/7.30 - 큐/요세푸스 문제.py
@@ -1,10 +1,10 @@
 n, k = map(int, input().split())
 result = []
+
 qu = [i for i in range(n, 0, -1)]
 while qu:
     for _ in range(k-1):
         t = qu.pop()
         qu = [t] + qu
-    result.append(qu.pop())
-
-print('<', (str(i)+', ' for i in result), '>', sep='')
+    result.append(str(qu.pop()))
+print('<' + ', '.join(result) + '>')
