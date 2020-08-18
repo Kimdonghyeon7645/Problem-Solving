5e02307b9b5608367b4571a72f331310524d6216
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-08-18 13:08:25 +0900

    refact : refactoring 체육복.py

--- 프로그래머스 Level 1/체육복.py
+++ 프로그래머스 Level 1/체육복.py
@@ -1,11 +1,9 @@
 def solution(n, lost, reserve):
-    lost_ = lost.copy()
-    for pp in lost_:
+    for pp in lost.copy():
         if pp in reserve:
             lost.remove(pp)
             reserve.remove(pp)
-    lost_ = lost.copy()
-    for people in lost_:
+    for people in lost.copy():
         for mazin in reserve:
             if mazin+1 == people or mazin-1 == people:
                 lost.remove(people)
