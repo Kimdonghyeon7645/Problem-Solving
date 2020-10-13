372146e31c1bbfc65a144e2eeadf41be9e558d7c
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-10-13 23:01:24 +0900

    code : update 삼각 달팽이.py

--- 프로그래머스 Level 2/삼각 달팽이.py
+++ 프로그래머스 Level 2/삼각 달팽이.py
@@ -1,3 +1,4 @@
+"""
 def solution(n):
     answer = [[0] * (i + 1) for i in range(n)]
     y, x = -1, 0
@@ -10,9 +11,18 @@ def solution(n):
             answer[y][x] = num
             num += 1
     return [ele for li in answer for ele in li]     # 2차원 -> 1차원 배열로 피는 기술
+"""
 
 
-
+# 파이썬 3.8의 ':=' 활용
+def solution(n):
+    answer = [[0] * (i + 1) for i in range(n)]
+    y, x, num = -1, 0, 1
+    for i in range(n, 0, -1):
+        y_p, x_p = [(1, 0), (0, 1), (-1, -1)][(n-i) % 3]
+        for _ in range(i):
+            answer[(y := y + y_p)][(x := x + x_p)] = (num := num + 1)
+    return [ele for li in answer for ele in li]
 
 
 # 테스트 코드
