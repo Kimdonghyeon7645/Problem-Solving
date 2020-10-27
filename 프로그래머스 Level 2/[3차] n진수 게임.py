ada37c322774a2b4980ceb4b11ba1da5a8d3ea28
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-10-27 15:26:09 +0900

    code : add [3차] n진수 게임.py

--- 프로그래머스 Level 2/[3차] n진수 게임.py
+++ 프로그래머스 Level 2/[3차] n진수 게임.py
@@ -1,7 +1,19 @@
 def solution(n, t, m, p):
-    answer = ''
-    return answer
+    i, answer = [0], []
+    while len(answer) < p-1 + t*m:
+        answer.extend(list(map(str, reversed(i))))
+        i[0] += 1
+        for index, ele in enumerate(i):
+            if ele >= n:
+                if i[-1]:
+                    i.append(0)
+                i[index], i[index+1] = ele % n, ele//n + i[index+1]
+    return answer, ''.join([(i if int(i) < 10 else chr(int(i) + 55)) for i in answer[p-1:(p-1+t*m):m]])
 
 
 # 테스트 코드
-
+# print(solution(2, 4, 2, 1))
+# print(solution(16, 16, 2, 1))
+# print(solution(16, 16, 2, 2))
+print(solution(8, 8, 2, 2))
+print(solution(16, 1000, 100, 1))
