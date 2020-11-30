7d175002c421974e61f6c941c2a7f0d91853122c
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-11-30 22:01:15 +0900

    code : add [3차] 파일명 정렬.py

---
+++ 프로그래머스/프로그래머스 Level 2/[3차] 파일명 정렬.py
@@ -0,0 +1,25 @@
+def solution(files):
+    answer = []
+    for i, file in enumerate(files):
+        answer.append([])
+        s = 0
+        while True:
+            s += 1
+            print(s, file[s])
+            if len(answer[i]) == 0 and file[s].isdigit():
+                answer[i].append(file[:s])
+                file = file[s:]
+                s = 0
+            elif len(answer[i]) == 1 and not file[s].isdigit():
+                answer[i].extend([file[:s], file[s:]])
+                break
+            if s == len(file)-1:
+                answer[i].append(file[:s+1])
+                break
+    print(answer)
+    return [''.join(ele) for ele in sorted(answer, key=lambda x: (x[0].upper(), int(x[1])))]
+
+
+# 테스트 코드
+# print(solution(["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]))
+print(solution(["img000012345", "img1.png", "img2", "IMG02"]))
