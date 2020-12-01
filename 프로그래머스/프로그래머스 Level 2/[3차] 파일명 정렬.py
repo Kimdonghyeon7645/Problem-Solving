f9c831503fdcff9b9e9e30d446fd0a8de6f16513
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-12-01 13:05:42 +0900

    code : add [3차] 파일명 정렬.py

--- 프로그래머스/프로그래머스 Level 2/[3차] 파일명 정렬.py
+++ 프로그래머스/프로그래머스 Level 2/[3차] 파일명 정렬.py
@@ -1,33 +1,36 @@
+# def solution(files):
+#     answer = []
+#     for i, file in enumerate(files):
+#         answer.append([])
+#         s = 0
+#         while True:
+#             s += 1
+#             print(s, file[s])
+#             if len(answer[i]) == 0 and file[s].isdigit():
+#                 answer[i].append(file[:s])
+#                 file = file[s:]
+#                 s = 0
+#             elif len(answer[i]) == 1 and not file[s].isdigit():
+#                 answer[i].extend([file[:s], file[s:]])
+#                 break
+#             if s == len(file)-1:
+#                 answer[i].append(file[:s+1])
+#                 break
+#     print(answer)
+#     return [''.join(ele) for ele in sorted(answer, key=lambda x: (x[0].upper(), int(x[1])))]
+
 def solution(files):
-    answer = []
-    for i, file in enumerate(files):
-        answer.append([])
-        s = 0
-        while True:
-            s += 1
-            print(s, file[s])
-            if len(answer[i]) == 0 and file[s].isdigit():
-                answer[i].append(file[:s])
-                file = file[s:]
-                s = 0
-            elif len(answer[i]) == 1 and not file[s].isdigit():
-                answer[i].extend([file[:s], file[s:]])
-                break
-            if s == len(file)-1:
-                answer[i].append(file[:s+1])
-                break
-    print(answer)
-    return [''.join(ele) for ele in sorted(answer, key=lambda x: (x[0].upper(), int(x[1])))]
+    import re
 
-
-# def solution(files):    # 위같이 정규표현식이 느리다고 쓰지 않는 것보다 지금처럼 정규 표현식을 사용하는 것이 더 빠르다.
-#     import re
-#
-#     return [''.join(i) for i in sorted(map(lambda x: re.match("([a-zA-Z]+)([0-9]+)([^0-9]*)", x).groups(), files),
-#                                        key=lambda m: (m[0].upper(), int(m[1])))]
+    return [''.join(i) for i in sorted(map(lambda x: re.match(r"([^\d]+)([\d]+)([^\n]*)", x).groups(), files), key=lambda m: (m[0].upper(), int(m[1])))]
+"""
+정규표현식, 느린줄만 알았지만, 이런 경우에선 정규표현식이 2~3배 더 효율이 좋았다.
+그리고, 본의아니게 한줄 코딩할 정도로 정규표현식은 강력했다...
+"""
 
 
 # 테스트 코드
 print(solution(["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]))
+print(solution(["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"]))
 print(solution(["img000012345", "img1.png", "img2", "IMG02"]))
 print(solution(["img000012345", "abc123defg123.jpg"]))
