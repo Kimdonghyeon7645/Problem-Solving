e3f3095c47843cc49c09bf51579d6f0bac7799c5
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-12-03 10:40:42 +0900

    code : update (외부도움)땅따먹기.py

--- 프로그래머스/프로그래머스 Level 2/(외부도움)땅따먹기.py
+++ 프로그래머스/프로그래머스 Level 2/(외부도움)땅따먹기.py
@@ -1,10 +1,22 @@
+# def solution(land):
+#     for row in range(len(land) - 2, -1, -1):
+#         # print(land[row], end='->')
+#         for col in range(4):
+#             land[row][col] += max([land[row + 1][i] for i in range(4) if i != col])
+#     # print(land[row])
+#     return max(land[0])
+
+
 def solution(land):
-    for row in range(len(land) - 2, -1, -1):
-        # print(land[row], end='->')
+    for row in range(len(land)-2, -1, -1):
         for col in range(4):
-            land[row][col] += max([land[row + 1][i] for i in range(4) if i != col])
-    # print(land[row])
+            land[row][col] += max(land[row+1][:col] + land[row+1][col+1:])
     return max(land[0])
+"""
+위처럼 리스트 내포로 검출해주는 방식은 효율성에서 350ms,
+아래처럼 리스트 슬라이싱 방식은 효율성에서 200ms가 나온다.
+(은근 리스트 슬라이싱 방식이 이럴때는 좋은 효율을 가지는 것을 기억하자.)
+"""
 
 
 # 테스트 코드
