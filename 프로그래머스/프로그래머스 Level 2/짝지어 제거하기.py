5041c2ea73cbc5d2159cdf43d0d607bc6b43b282
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-12-05 23:22:25 +0900

    code : finish 짝지어 제거하기.py

--- 프로그래머스/프로그래머스 Level 2/짝지어 제거하기.py
+++ 프로그래머스/프로그래머스 Level 2/짝지어 제거하기.py
@@ -18,7 +18,23 @@ del 은 시간복잡도를 많이 잡아먹으며, 이렇게 코드를 짜는 �
 
 
 def solution(s):
-    pass
+    li = list(reversed(s))
+    stack = [li.pop()]
+    while li:
+        target = li.pop()
+        if stack and target == stack[-1]:
+            del stack[-1]
+        else:
+            stack.append(target)
+        print(stack, li[::-1])
+    return 0 if stack else 1
+"""
+와...
+딱 7분 만에 문제가 풀렸다. 내가 생각했던게 한번에 맞았다. 내가 알고리즘 수업 때 배운 방법이 맞은 것이다...
+확실하게 이번 문제로 배운 것은, 나 자신에 대한 자만보다도, 알고리즘 접근 방식을 배울 필요가 있다는 것이였다. 
+마치 가장 효율적이라 발견된 자료구조를 배우듯이 말이다.
+"""
+# TODO 알고리즘 책 읽고 정리해뵈기!
 
 
 # 테스트 코드
