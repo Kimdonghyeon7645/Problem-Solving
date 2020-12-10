0f729a6cf885fa054b3a3467eefac3a3fa57ea44
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-12-10 20:06:59 +0900

    code : add 소수 찾기.py

--- 프로그래머스/프로그래머스 Level 2/소수 찾기.py
+++ 프로그래머스/프로그래머스 Level 2/소수 찾기.py
@@ -1,13 +1,22 @@
 def solution(n):
-    maximum = int(''.join(sorted(n, reverse=True)))
-    so_su = list(range(2, maximum+1))
-    i = 0
-    while i < len(so_su):
-        so_su = so_su[:i+1] + [e for e in so_su[i+1:] if e % so_su[i] != 0]
-        i += 1
-        print(so_su)
-    return
+    index_li = [list(map(str, range(len(n))))]
+    for i in range(len(n)-1):
+        index_li.append([])
+        for index in index_li[i]:
+            index_li[i+1].extend([index+str(ele) for ele in range(len(n)) if str(ele) not in index])
+    word_set = set(int(''.join(n[int(j)] for j in i)) for li in index_li for i in li)
+    word_set -= {0, 1}
+    answer = len(word_set)
+    for word in word_set:
+        for i in range(2, int(word ** 1/2)+1):
+            if word % i == 0:
+                answer -= 1
+                break
+    return answer
 
 
 # 테스트 코드
-print(solution("100"))
+print(solution("71"))
+print(solution("101"))
+# print(solution("000001"))
+print(solution("9999999"))
