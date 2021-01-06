74cfedfc62db11524116d91877c00d99fa8f44aa
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-01-06 20:11:00 +0900

    code : add 야근 지수.py

--- 프로그래머스/프로그래머스 Level 3/야근 지수.py
+++ 프로그래머스/프로그래머스 Level 3/야근 지수.py
@@ -6,6 +6,8 @@ def solution(n, works):
     :return:으론, 처리할 일 각각을 제곱한 값을 모두 더한 값의 최소값을 출력하면 된다.
     그니까 works 숫자 배열에서 n개 만큼을 빼서, 남은 works 요소들의 제곱을 모두 더한 값의 최소값이 나오면 된다.
     """
+
+    # way 1. 간단하게 풀었지만, 효율성(시간복잡도)에서 당연하게도 걸린다.
     for _ in range(n):
         works[works.index(max(works))] -= 1
     return sum(i ** 2 for i in works if i > -1)
