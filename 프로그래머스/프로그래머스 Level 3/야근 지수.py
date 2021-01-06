3db0b50db8b2ba395e0bc303a9396ad9e956f590
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-01-06 21:21:35 +0900

    code : update 야근 지수.py

--- 프로그래머스/프로그래머스 Level 3/야근 지수.py
+++ 프로그래머스/프로그래머스 Level 3/야근 지수.py
@@ -7,12 +7,21 @@ def solution(n, works):
     그니까 works 숫자 배열에서 n개 만큼을 빼서, 남은 works 요소들의 제곱을 모두 더한 값의 최소값이 나오면 된다.
     """
 
-    # way 1. 간단하게 풀었지만, 효율성(시간복잡도)에서 당연하게도 걸린다.
-    for _ in range(n):
-        works[works.index(max(works))] -= 1
-    return sum(i ** 2 for i in works if i > -1)
+    # # way 1. 간단하게 풀었지만, 효율성(시간복잡도)에서 당연하게도 걸린다.
+    # for _ in range(n):
+    #     works[works.index(max(works))] -= 1
+    # return sum(i ** 2 for i in works if i > -1)
+
+    # way 2. 정렬해서 활용
+    works.sort()
+    for _ in range(min(n, sum(works))):
+        works[-1] -= 1
+        if works[-1] < works[-2]:
+            works.sort()
+    return sum(i ** 2 for i in works)
 
 
 # 테스트 코드
 print(solution(4, [4, 3, 3]))
+print(solution(1, [2, 1, 2]))
 
