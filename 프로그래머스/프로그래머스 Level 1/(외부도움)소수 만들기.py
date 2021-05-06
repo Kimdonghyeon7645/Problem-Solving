f0bbecee985bc77962efeebd28212b35a5bc87f6
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-05-06 20:20:31 +0900

    code : add (외부도움)소수 만들기.py

--- 프로그래머스/프로그래머스 Level 1/(외부도움)소수 만들기.py
+++ 프로그래머스/프로그래머스 Level 1/(외부도움)소수 만들기.py
@@ -51,7 +51,18 @@
 
 
 def solution(nums):
-    pass
+    """
+    아니 소수 만들기라고 해서 모든 경우의 수를 시간 효율적이게 하려고 아둥바둥 했었었다;;
+    근데 걍 3중 for 문으로 해결될 일이였다...
+    """
+    answer = 0
+    for num in [nums[i]+nums[j]+nums[k] for i in range(len(nums)-2) for j in range(i+1, len(nums)-1) for k in range(j+1, len(nums))]:
+        for i in range(2, int(num ** 1/2)+1):
+            if num % i == 0:
+                answer -= 1
+                break
+        answer += 1
+    return answer
 
 
 # 테스트 코드
